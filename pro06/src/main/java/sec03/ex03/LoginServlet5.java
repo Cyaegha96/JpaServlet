8c43cbec65d42288591c4009c313badbeba4763e
Cyaegha96 <zazae51@gmail.com>
2021-12-07 16:02:45 +0900

    Form - loginServlet Test

--- pro06/src/main/java/sec03/ex03/LoginServlet5.java
+++ pro06/src/main/java/sec03/ex03/LoginServlet5.java
@@ -1,4 +1,4 @@
-package sec01.ex01;
+package sec03.ex03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,50 +11,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet2
+ * Servlet implementation class LoginServlet5
  */
-@WebServlet("/login2")
-public class LoginServlet2 extends HttpServlet {
+@WebServlet("/login5")
+public class LoginServlet5 extends HttpServlet {
 	//private static final long serialVersionUID = 1L;
-
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
+ 
 	public void init(ServletConfig config) throws ServletException {
 		System.out.println("init 메서드 호출");
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
+
 	public void destroy() {
-		System.out.println("destroy 메서드 호출");
+		System.out.println("destroy메서드 호출");
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-		
 		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		String address = request.getParameter("user_address");
+		System.out.println("아이디: "+id);
+		System.out.println("비밀번호: "+pw);
+		System.out.println("주소: "+address);
 		
-		String user_id = request.getParameter("user_id");
-		String user_pw = request.getParameter("user_pw");
-		
+
 		String data = "<html>";
 		data += "<body>";
-		data += "아이디: " + user_id;
+		data += "아이디: " + id;
 		data += "<br>";
-		data += "비밀번호: " + user_pw;
+		data += "비밀번호: " + pw;
+		data += "<br>";
+		data += "주소: " +address;
 		data += "</body>";
 		data += "</html>";
 		
 		out.print(data);
-		
 	}
 
 }
---
+++ pro06/src/main/java/sec04/ex01/LoginTest2.java
@@ -0,0 +1,62 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginTest2
+ */
+@WebServlet("/loginTest2")
+public class LoginTest2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("아이디: "+user_id);
+		System.out.println("비밀번호: "+user_pw);
+		
+		String html = "<html><body>";
+		if(user_id != null && (user_id.length() !=0)) {
+
+			if(user_id.equals("admin")) {
+
+				html += "관리자로 로그인 하셨습니다.";
+				html += "<br>";
+				html += "<input type=button value = '회원정보 수정하기'>";
+				html += "<input type=button value = '회원정보 삭제하기'>";
+				html += "</body>";
+				html += "</html>";
+				out.print(html);
+			}else {
+
+				html += user_id + "님!! 로그인 하셨습니다.";
+				html += "</body>";
+				html += "</html>";
+				out.print(html);
+			}
+			
+		}else {
+			html += "아이디를 입력하세요!";
+			html += "<br>";
+			html += "<a href='http://localhost:8080/pro06/test01/login.html'>로그인창으로 이동<a/>";
+			html += "</body>";
+			html += "</html>";
+			out.print(html);
+		}
+		
+	}
+
+}
